7d76b104520cce5806c0e5c4733e439b47398348
Martin Julian <69302977+mjcaniceto@users.noreply.github.com>
2022-04-18 10:58:36 +0800

    Add files via upload

---
+++ LR2/generator_modified.py
@@ -0,0 +1,58 @@
+"""
+Program: generator.py
+Author: Ken
+Generates and displays sentences using a simple grammar
+and vocabulary.  Words are chosen at random.
+"""
+
+import random
+import os
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
+
+def getWords(fileName):
+    """Gets words out of files insted of declaring in the program"""
+    fileWords = open(fileName)
+    tempList = list()
+    for aWord in fileWords:
+        aWord = aWord.strip()
+        tempList.append(aWord)
+    words = tuple(fileWords)    
+    fileWords.close()
+
+    return words
+
+articles = getWords('articles.txt')
+nouns = getWords('nouns.txt')
+verbs = getWords('verbs.txt')
+prepositions = getWords('prepositions.txt')
+
+def sentence():
+    """Builds and returns a sentence."""
+    return nounPhrase() + " " + verbPhrase()
+
+def nounPhrase():
+    """Builds and returns a noun phrase."""
+    return random.choice(articles) + " " + random.choice(nouns)
+
+def verbPhrase():
+    """Builds and returns a verb phrase."""
+    return random.choice(verbs) + " " + nounPhrase() + " " + \
+           prepositionalPhrase()
+
+def prepositionalPhrase():
+    """Builds and returns a prepositional phrase."""
+    return random.choice(prepositions) + " " + nounPhrase()
+
+def main():
+    """Allows the user to input the number of sentences
+    to generate."""
+    number = int(input("Enter the number of sentences: "))
+    for count in range(number):
+        print(sentence())
+
+# The entry point for program execution
+if __name__ == "__main__":
+    main()
