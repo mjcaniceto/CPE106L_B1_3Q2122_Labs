1ae8ad6b6727eb1438ad5ebfc0efb003635cf080
Martin Julian <69302977+mjcaniceto@users.noreply.github.com>
2022-04-27 01:57:13 +0800

    Update projectMain.py

--- Project/projectMain.py
+++ Project/projectMain.py
@@ -1,1 +1,29 @@
+import tkinter as tk
+import random
+import gameColors as color
 
+class Game(tk.Frame):
+   
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.master.title("2048 Game")
+ 
+        self.mainGrid = tk.Frame(
+            self, bg = color.board_color, bd = 3, width = 600, height = 600
+        )
+        self.mainGrid.grid(pady = (110,0))
+ 
+        self.GUI()
+        self.startGame()
+ 
+        self.mainloop()
+  
+  
+  
+
+def main():
+    Game()
+
+if __name__ == "__main__":
+    main()
