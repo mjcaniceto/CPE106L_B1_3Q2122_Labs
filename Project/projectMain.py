d9eb9c2a0380824e8a25f5364e859afe9589b1a7
Patrick John Figarola <100175636+PINYANG0@users.noreply.github.com>
2022-05-03 11:43:47 +0800

    Update projectMain.py

--- Project/projectMain.py
+++ Project/projectMain.py
@@ -78,7 +78,46 @@ def startGame(self):
 
         self.score = 0
   
-  
+def merge(self):
+        for i in range(4):
+            for j in range(3):
+                if self.matrix[i][j] != 0 and self.matrix[i][j] == self .matrix[i][j + 1]:
+                    self.matrix[i][j] *= 2
+                    self.matrix[i][j + 1] = 0
+                    self.score += self.matrix[i][j]
+
+    def shift(self):
+        Matrix_1 = []
+        for i in range(4):
+            Matrix_1.append([])
+            for j in range(4):
+                Matrix_1[i].append(self.matrix[i][3-j])
+        self.matrix = Matrix_1
+    
+    def stack(self):
+        Matrix_1 = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            fillPosition = 0
+            for j in range(4):
+                if self.matrix[i][j] != 0:
+                    Matrix_1[i][fillPosition] = self.matrix[i][j]
+                    fillPosition += 1
+        self.matrix = Matrix_1
+
+    def transpose(self):
+        Matrix_1 = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            for j in range(4):
+                Matrix_1[i][j] = self.matrix[j][i]
+        self.matrix = Matrix_1
+
+    def addRandomTile(self):
+        row = random.randint(0,3)
+        col = random.randint(0,3)
+        while(self.matrix[row][col] != 0):
+            row = random.randint(0,3)
+            col = random.randint(0,3)
+        self.matrix[row][col] = random.choice([2,4])  
 
 def main():
     Game()
